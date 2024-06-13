4eccf9b8abdd6253df4807ea394e6789d2a35487
Saad10922 <saadq10922@gmail.com>
2024-06-13 18:49:37 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,143 +1,130 @@
 #include<iostream>
 #include<WinSock2.h>
-#include<WS2tcpip.h>
-#include<tchar.h>
+#include<ws2tcpip.h>
 #include<thread>
-#include<vector>
-#include<mutex>
-#include<algorithm>
+#include<string>
 #include<Windows.h>
 
 using namespace std;
-#pragma comment(lib,"ws2_32.lib")//for linking window Socket Library
+
+#pragma comment(lib, "ws2_32.lib")
+
 /*
-<==Steps for Writting Server Code==>
-
-//Initilize winsock library
-//create the socket
-//get the ip and port
-//bind the ip/port in socket
-//listen on the socket
-//accept
-//receive and send
-//close the socket
-// cleanup the winsock
-
-
-
+Steps Involved in Client Creation
+ create Socket
+ connect to the server
+ send/recv
+ Close the  Socket
 
 */
-bool Initilize() {
+bool Initialize()
+{
     WSADATA data;
-    return WSAStartup(MAKEWORD(2, 2), &data) == 0; //Version 2.2
+    return WSAStartup(MAKEWORD(2, 2), &data) == 0;
 }
 void PrintHeader() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << "**************************************************" << endl;
-    cout << "*              Welcome to Chat Server             *" << endl;
+    cout << "*              Welcome to Chat Client             *" << endl;
     cout << "**************************************************" << endl;
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
 }
-
 void PrintDivider() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << "--------------------------------------------------" << endl;
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
 }
+void SendMsg(SOCKET s)
+{
 
-void IntractWithClient(SOCKET clientSocket, vector<SOCKET>& clients, mutex& mtx) {
-    cout << "Client Connected" << endl;
-    char buffer[4096];
+    cout << "Enter Your Nick Name: "<<endl;
 
-    while (true) {
-        int bytesrecvd = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesrecvd <= 0) {
-            cout << "Client Disconnected" << endl;
+    string name;
+    getline(cin, name);
+    string message;
+
+    while (true)
+    {
+        getline(cin, message);
+        string msg = name + " : " + message;
+        int bytesent = send(s, msg.c_str(), msg.length(), 0);
+        if (bytesent == SOCKET_ERROR)
+        {
+            cout << "ERROR IN SENDING MESSAGE" << endl;
             break;
         }
-        string message(buffer, bytesrecvd);
-        cout << "Message from client: " << message << endl;
 
-        lock_guard<mutex> lock(mtx); // Lock the mutex for safe access to clients
-        for (auto client : clients) {
-            if (client != clientSocket) {
-                send(client, message.c_str(), message.length(), 0);
-            }
+        if (message == "quit")
+        {
+            cout << "STOPPING THE Application ........" << endl;
+            break;
         }
     }
-
-    {
-        lock_guard<mutex> lock(mtx); // Lock the mutex for safe access to clients
-        auto it = find(clients.begin(), clients.end(), clientSocket);
-        if (it != clients.end())
-            clients.erase(it);
-    }
-    closesocket(clientSocket);
+    closesocket(s);
+    WSACleanup();
 }
 
-int main() {
-    PrintHeader();
+void ReceiveMsg(SOCKET s)
+{
+    char buffer[4096];
+    int recvlength = 0;
 
-    if (!Initilize()) {
-        cout << "winsock initilization failed" << endl;
+    while (true)
+    {
+        recvlength = recv(s, buffer, sizeof(buffer), 0);
+        if (recvlength <= 0)
+        {
+            cout << "DISCONNECTED FROM THE SERVER" << endl;
+            break;
+        }
+        cout << "Server: " << string(buffer, recvlength) << endl;
+    }
+    closesocket(s);
+    WSACleanup();
+}
+
+int main()
+{
+    PrintHeader();
+    cout << "Client program started" << endl;
+
+    if (!Initialize())
+    {
+        cout << "Initialize winsock Failed" << endl;
         return 1;
     }
 
-    cout << "Server Program Started" << endl;
-    SOCKET listenSocket = socket(AF_INET /*Using ipv4*/, SOCK_STREAM/*TCP*/, 0);
-    if (listenSocket == INVALID_SOCKET) {
-        cout << "socket creation failed" << endl;
+    SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s == INVALID_SOCKET)
+    {
+        cout << "INVALID SOCKET CREATED" << endl;
         return 1;
     }
 
     int port = 12345;
+    string serveraddress = "127.0.0.1";
     sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(port);
-    if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1) {
-        cout << "Setting address structure failed" << endl;
-        closesocket(listenSocket);
+    inet_pton(AF_INET, serveraddress.c_str(), &(serveraddr.sin_addr));
+
+    if (connect(s, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR)
+    {
+        cout << "NOT ABLE TO CONNECT TO SERVER" << endl;
+        closesocket(s);
         WSACleanup();
         return 1;
     }
 
-    if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR) {
-        cout << "bind failed" << endl;
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
+    cout << "SUCCESSFULLY CONNECTED TO SERVER" << endl;
 
-    if (listen(listenSocket, SOMAXCONN /*amount of request it will put in the queue*/) == SOCKET_ERROR) {
-        cout << "listen failed" << endl;
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
+    thread senderthread(SendMsg, s);
+    thread receiver(ReceiveMsg, s);
 
-    cout << "server has started listening on port: " << port << endl;
-    vector<SOCKET> clients;
-    mutex mtx;
+    senderthread.join();
+    receiver.join();
 
-    while (true) {
-        SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
-        if (clientSocket == INVALID_SOCKET) {
-            cout << "invalid client socket " << endl;
-            continue;
-        }
-
-        {
-            lock_guard<mutex> lock(mtx); // Lock the mutex for safe access to clients
-            clients.push_back(clientSocket);
-        }
-
-        thread t1(IntractWithClient, clientSocket, ref(clients), ref(mtx));
-        t1.detach();
-    }
-
-    closesocket(listenSocket);
-    WSACleanup();
     return 0;
 }
